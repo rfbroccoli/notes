fc2e30e90c3d9ef6d717c27802ddef1e87965845
rfbroccoli <rfbroccoli@gmail.com>
2022-02-28 12:59:08 +0000

    added for in arr & for in obj & for of arr

--- loops.js
+++ loops.js
@@ -1,4 +1,35 @@
+
+// Normal Loop
 for (let i = 5; i < 10; i += 1) {
     console.log("hello")
+    console.log(i)
+}
+
+const array = ["a nyein", "allina ray", "aung", "daniel mcConaughey"]
+
+const object = {
+    name: "learn to code",
+    teacher: "broccoli"
+}
+
+// for in array
+for (let i in array) {
+    console.log(i)
+}
+
+// object.name
+// object["name"]
+
+// for in obj 
+for (let i in object) {
+    console.log(i)
+    console.log(object[i])
+}
+
+
+
+// for of
+for (let i of array) {
+
     console.log(i)
 }
